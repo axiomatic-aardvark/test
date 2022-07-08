a6b01a322a72d11a52987ea8bcfac6c9cc520584
axiomatic-aardvark <pavlovskipetko@protonmail.com>
2022-07-08 13:05:11 +0300

    Changes.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  // some change
+  // some changeee
   return (
     <div className="App">
       <header className="App-header">
